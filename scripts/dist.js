7b53921d66d9439fa243c158308a36524ffff432
William Novak <williamnvk@gmail.com>
2020-06-02 10:10:07 -0300

    add dist script, remove sass-dart modules usage

--- scripts/dist.js
+++ scripts/dist.js
@@ -1,36 +1,63 @@
 #!/usr/bin/env node
 const execSync = require('child_process').execSync;
+var pjson = require('../package.json');
 
 var readline = require('readline-sync');
-var encoding = { encoding: 'utf-8' }
+var encoding = { encoding: 'utf-8' };
 var color = '\x1b[36m%s\x1b[0m';
+var distFolder = 'dist/';
+var themes = ['light', 'dark'];
+
+console.clear()
 
 console.log(color, `
-==================================================
-
 CAVILHA UI 'dist' task running
+--------------------------------------------------
+1/3 Cleaning ${distFolder} files`)
 
---------------------------------------------------`)
+execSync(`rimraf ${distFolder.replace('/', '')}`, encoding)
 
-var cleanFolder = readline.question('Clear `dist` folder? (y/n): ');
-
-if (cleanFolder !== 'n') {
-  execSync('npm run clean', encoding);
-}
-
-let files = ['light', 'dark'];
-
-files.map(function(file, index){
+themes.map(function(theme, index){
   try {
-    console.log(color, `${index + 1}/${files.length} Generating css/${file} files
-  --------------------------------------------------`)
-    execSync(`sass --no-source-map sass/${file}.sass:dist/themes/${file}.css`, encoding);
-    execSync(`sass --no-source-map sass/${file}.sass:dist/themes/${file}.min.css --style compressed`, encoding);
-    execSync(`postcss --use=autoprefixer --map false --output dist/themes/${file}.css dist/themes/${file}.css`, encoding);
-    execSync(`postcss --use=autoprefixer --map false --output dist/themes/${file}.min.css dist/themes/${file}.min.css`, encoding);
+
+    var inputFile = `sass/${theme}.sass`
+    var outputFile = `${distFolder}themes/${theme}.css`;
+    var outputMinFile = `${distFolder}themes/${theme}.min.css`;
+    var files = [outputFile, outputMinFile]
+
+    console.log(color, `${(index + 1)+ 1}/3 Generating ${outputFile} file
+    --------------------------------------------------`)
+
+    // generate dist
+    execSync(`node-sass --output-style expanded ${inputFile} ${outputFile}`, encoding)
+    execSync(`node-sass --output-style compressed ${inputFile} ${outputMinFile}`, encoding)
+
+    // prefix
+    execSync(`postcss --use=autoprefixer --map false --output ${outputFile} ${outputFile}`, encoding);
+    execSync(`postcss --use=autoprefixer --map false --output ${outputMinFile} ${outputMinFile}`, encoding);
+
+    // clean
+    execSync(`cleancss -o ${outputMinFile} ${outputMinFile}`, encoding);
+
+    execSync(`rimraf ${outputFile}.map`, encoding);
+    execSync(`rimraf ${outputMinFile}.map`, encoding);
+
+    files.map(function(file){
+      var fs = require('fs')
+      fs.readFile(file, 'utf8', function (err,data) {
+        var result = data.replace(/<version>/g, `v${pjson.version}`);
+        fs.writeFile(file, result, 'utf8', function (err) {
+          if (err) return console.log(err);
+        });
+      });
+    })
+
   } catch (e) {
-    //
+    console.error(e);
   }
 })
 
-var cleanFolder = readline.question("Do you want to publish to npm? (y/n): ");
+// var cleanFolder = readline.question("Do you want to publish to npm? (y/n): ");
+
+
+
